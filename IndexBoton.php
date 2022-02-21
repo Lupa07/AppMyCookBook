955baf5ec634db141b4c12e93eea1448d20d34eb
Joaquin <joaquin.garcia.jimenez@gmail.com>
2022-02-21 19:43:56 +0100

    responsive y tamaño index bueno

--- IndexBoton.php
+++ IndexBoton.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   
@@ -77,10 +78,10 @@
     <div id="contenedor">
       <div id="corporativo">
         <div id="logo">
-            <img class="mb-4" src="img/favIcon.png" alt="" width="72" height="57" />
+            <img class="mb-4" src="img/favIcon.png" alt="" width="150" height="150" />
         </div>
         <div id="texto">
-            <h1 class="h3 mb-3 fw-normal">My CookBook <br />Iniciar Sesion</h1>
+            <h1 class="h2 mb-3 fw-normal">My CookBook <br />Iniciar Sesion</h1>
         </div>
         </div>
       <div class="botones">
@@ -112,7 +113,7 @@
       dialogo.close();
     })
   </script>
-  <br><br><br><br>
+
    <?php include "templates/footer.php"; ?> 
   </body>
   
